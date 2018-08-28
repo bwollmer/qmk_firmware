fed21908cf2b3ed353d19371ddac3ec17428ef1e
Benjamin <2wollmer@informatik.uni-hamburg.de>
2018-08-29 01:42:40 +0200

    moved page up/down buttons

--- keyboards/ergodox_ez/keymaps/bwollmer/keymap.c
+++ keyboards/ergodox_ez/keymaps/bwollmer/keymap.c
@@ -29,11 +29,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *   | ~L1  |  '   |      | LALT | CTRL |                                       | RAlt | Win  |      |      | ~L1  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        |      | Win  |       |      |      |
+ *                                        |PgDown| Win  |       |      | PgUp |
  *                                 ,------|------|------|       |------+--------+------.
- *                                 |      |      | Home |       | PgUp |        |      |
+ *                                 |      |      | Home |       |      |        |      |
  *                                 | Space|Backsp|------|       |------|  ~L1   |Enter |
- *                                 |      |ace   | End  |       | PgDn |        |      |
+ *                                 |      |ace   | End  |       |      |        |      |
  *                                 `--------------------'       `----------------------'
  */
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
@@ -45,18 +45,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,         KC_A,         KC_S,     KC_D,    KC_F,     KC_G,
         KC_LSFT,        KC_Z,         KC_X,     KC_C,    KC_V,     KC_B,   KC_ESC,
         KC_FN1,        KC_QUOT,      KC_TRNS,  KC_LALT, KC_LCTRL,
-                                               KC_TRNS,  KC_RGUI,
-                                                              KC_HOME,
-                                               KC_SPC,KC_BSPC,KC_END,
+                                                       KC_PGDN, KC_RGUI,
+                                                                KC_HOME,
+                                               KC_SPC, KC_BSPC, KC_END,
         // right hand
              KC_TRNS,      KC_6,   KC_7,    KC_8,    KC_9,    KC_0,              KC_MINS,
              TG(SYMB),     KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,              KC_EQL,
                            KC_H,   KC_J,    KC_K,    KC_L,    LT(MDIA, KC_SCLN), KC_QUOT,
              MEH_T(KC_NO), KC_N,   KC_M,    KC_COMM, KC_DOT,  CTL_T(KC_SLSH),    RSFT_T(KC_BSLS),
                                    KC_RALT, KC_RGUI, KC_TRNS, KC_TRNS,           KC_FN1,
-             KC_TRNS,        KC_TRNS,
-             KC_PGUP,
-             KC_PGDN, KC_FN1, KC_ENT
+             KC_TRNS, KC_PGUP,
+             KC_TRNS,
+             KC_TRNS, KC_FN1, KC_ENT
     ),
 /* Keymap 1: Symbol Layer
  *
